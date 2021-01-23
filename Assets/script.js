d41c9e5c023ac6f54583779799ab0927061a31df
Alvaro <astaboada7@gmail.com>
2021-01-23 18:29:06 +0100

    commit

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+
+let modal = document.getElementById("contactWindow");
+let button = document.getElementById("contact");
+let span = document.getElementsByClassName("close")[0];
+
+button.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
